80d41783f77d26b0a1f7d441664f3c7c83e1a6d2
Ertugrul Demir <ertdemir01@gmail.com>
2022-06-07 20:38:57 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Author: Ertuğrul Demir
 # This application should use for https://yordle.pages.dev
+from ast import Try
+from distutils.log import error
+from logging import exception
 from unicodedata import name
+from matplotlib.pyplot import show
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,7 +20,7 @@ class Champion:
     @property
     def length(self) -> str:
         return self.__length
-    
+
     @name.setter
     def name(self,new_name):
         self.__name = new_name
@@ -34,6 +38,12 @@ class Champion:
     def length(self):
         print("Deleted the length of"+self.length)
 
+class Letter:
+    def __init__(self,letter,index,type) -> None:
+        self.letter = letter
+        self.index = index
+        self.type = type
+
 
 def get_champs():
     champions=[]
@@ -47,21 +57,63 @@ def get_champs():
             champ_name = oneLink.split("/")[-2]
             champ_name = champ_name.replace("-","")
             champions.append(Champion(champ_name))
-    
+            
     return champions
+
+def short_by_len(champions,length):
+    lenSuitableChamps = []
+
+    if length != "0":
+        for Champ in champions:
+            if Champ.length == length:
+                lenSuitableChamps.append(Champ)
+    return lenSuitableChamps
+
+
+
+def eliminate_by_letters(champions,letters): #TODO
+    newChampions = []
+    for letter in letters:
+        for champ in champions:
+            a = 0
+
+
+    return champions
+
+def show_champions(champions):
+    for champ in champions:
+        print(champ.name)
+
+def error_check( inputValue , letterIndex , letterType,length):
+    if letterIndex >= length+1: 
+        print("Enter invalid index")
+        return True
+    if letterType not in ["green","yellow","gray"]:
+        print("Enter invalid color")
+        return True
+
+    
 
 def app():
     Champions = get_champs()
-    inputValue = ""
-    print("Enter 0 if you want exit anytime")
-    inputValue = input("Enter the number of boxes: ")
-    
 
-    while(True):
-        if str(inputValue) == "0":
-            break
+    print("Enter 0 if you want exit ")
+    inputValue = input("Enter the number of boxes  >")
+    Champions = short_by_len(Champions, inputValue)
+    length = inputValue
+    letters = []
+    while(inputValue != "0" ):
+        show_champions(Champions)
         
+        while(True):
+            inputValue , letterIndex , letterType  = input("(Enter ,, if want stop to enter letter) Enter color (green,yellow,gray) for type of letters and the letter with index. (Ex. >A,2,green) >").lower().split(",")
+            if error_check( inputValue , letterIndex , letterType, length):
+                break
+            if inputValue == "" :
+                break
+            letters.append(Letter(inputValue, int(letterIndex)-1, letterType)) #User enter the index 1,2,3 but we store 0,1,2
         
+        inputValue = input("Enter 0 for exit")
 
 
 if __name__ == '__main__':
